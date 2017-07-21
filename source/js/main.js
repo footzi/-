3e4276fbf81f9ed414dfd9c721e71daf7a157fed
footzi <footzi@mail.ru>
2017-07-21 07:08:25 +0300

    general changes

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
-//выбор либо снятия редио на детей
+//выбор либо снятие радио на детей
 $("#on-children").on("click", function() {
-    if($(this).is(":checked")) {
+    if ($(this).is(":checked")) {
         $(".children input:first").prop("checked", true);
     } else {
         $(".children input").prop("checked", false);
@@ -11,42 +11,52 @@ $("#on-children").on("click", function() {
 $(".children input").on("click", function() {
     $("#on-children").prop("checked", true);
 });
-$("#on-invalid").on("click", function() {
-    $(".invalid").fadeToggle();
+
+//скрытие/открытие полей
+$("#on-disabled").on("click", function() {
+    $(".disabled").fadeToggle();
+});
+$("#on-limitation").on("click", function() {
+    $(".limitation").fadeToggle();
+});
+$("#on-other").on("click", function() {
+    $(".other").fadeToggle();
 });
 
 $(".data input").change(function() {
-    var test = $(".children input").is(":checked");
-    var deductionsCh=0;
-    if(test) {
-        deductionsCh = $(".children input:checked").attr("data-deductions");
+    //вывод вычетов с детей
+    let deductionsCh,
+        deductionsDe,
+        deductionsLi,
+        deductionsOt;
 
+    deductionsCh = 0;
+    if ($(".children input").is(":checked")) {
+        deductionsCh = parseInt($(".children input:checked").attr("data-deductions"));
     };
 
-    console.log(deductionsCh);
-    
-    // if($("#on-children").is(":checked")) {
-            
-    //     let radio = $(".children input");
-    //     for (var i=1; i<radio.length; i++) {
-    //         deductionsCh = $(this).prop("data-deductions");
-    //         console.log(deductionsCh);
-    //     } 
+    //вывод вычетов с детей-инвалидов;
+    deductionsDe = parseInt($("#disabled").val() * 12000);
 
-     
+    //вывод остальных вычетов
+    deductionsLi = parseInt($("#limitation").val());
+    deductionsOt = parseInt($("#other").val());
 
 
-    
-
-
-
-    let salary = $("#salary").val()-deductionsCh;
-    let percent = salary * 13 / 100;
+    let salary = $("#salary").val();
+    let deductions = deductionsCh + deductionsDe + deductionsLi + deductionsOt;
+    console.log("общий=" + deductions);
+    console.log("дети=" + deductionsCh);
+    console.log("инвалиды=" + deductionsDe);
+    console.log("ограничения=" + deductionsLi);
+    console.log("другие=" + deductionsOt);
+    let percent = (salary - deductions) * 13 / 100;
     let hand = salary - percent;
 
     let pfr = salary * 22 / 100;
     let ffoms = salary * 5.1 / 100;
     let fss = salary * 2.9 / 100;
+    let fss2 = salary * 0.2 / 100;
 
 
 
@@ -55,4 +65,5 @@ $(".data input").change(function() {
     $("#pfr").html(pfr);
     $("#ffoms").html(ffoms);
     $("#fss").html(fss);
+    $("#fss2").html(fss2);
 });
